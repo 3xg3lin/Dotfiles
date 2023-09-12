cfcfc7fa011ce14a3b31a652a866b63f48e4040e
krm <73038148+3xg3lin@users.noreply.github.com>
2023-09-12 19:04:52 +0300

    Add files via upload

---
+++ .config/bg-changer.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+PICTURE=$(ls ~/Pictures |shuf -n 1)
+WHICHPIC=$(cat ~/.config/rofi/config.rasi|grep -o 'guweiz[0-9]*.jpg')
+sed -i "s/$WHICHPIC/$PICTURE/g" ~/.config/rofi/config.rasi ~/.config/rofi/powermenu/powerconfig.rasi
+feh --bg-fill  ~/Pictures/$PICTURE
