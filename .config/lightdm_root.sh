3e043d1324ab4135988afc60d5319ef5c0b8bcf3
krm <73038148+3xg3lin@users.noreply.github.com>
2023-09-13 12:50:18 +0300

    Create lightdm_root.sh

---
+++ .config/lightdm_root.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+PICTURE=$(ls /usr/share/backgrounds/archlinux/ |shuf -n 1)
+WHICHPIC=$(cat /etc/lightdm/lightdm-gtk-greeter.conf|grep -o 'guweiz[0-9]*.jpg')
+sed -i "s/$WHICHPIC/$PICTURE/g" /etc/lightdm/lightdm-gtk-greeter.conf
