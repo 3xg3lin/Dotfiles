6e13bbbccb948aa23c19a445a36e3e350654fc9d
krm <73038148+3xg3lin@users.noreply.github.com>
2023-09-13 12:42:19 +0300

    Add files via upload

---
+++ .config/background.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PICTURE=$(cat /etc/lightdm/lightdm-gtk-greeter.conf |grep -o "guweiz[0-9]*.jpg")
+WHICHPIC=$(cat ~/.config/rofi/config.rasi|grep -o 'guweiz[0-9]*.jpg')
+sed -i "s/$WHICHPIC/$PICTURE/g" ~/.config/rofi/config.rasi ~/.config/rofi/powermenu/powerconfig.rasi
+feh --bg-fill  ~/Pictures/$PICTURE
+betterlockscreen -u ~/Pictures/$PICTURE
