6ea55e81140f35ecc3183900ccb4b0a69d3dd032
krm <73038148+3xg3lin@users.noreply.github.com>
2023-09-13 11:45:26 +0300

    Update bg-changer.sh

--- .config/bg-changer.sh
+++ .config/bg-changer.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/bash
-PICTURE=$(ls ~/Pictures |shuf -n 1)
+#!/bin/bash
+PICTURE=$(cat /etc/lightdm/lightdm-gtk-greeter.conf |grep -o "guweiz[0-9]*.jpg")
 WHICHPIC=$(cat ~/.config/rofi/config.rasi|grep -o 'guweiz[0-9]*.jpg')
 sed -i "s/$WHICHPIC/$PICTURE/g" ~/.config/rofi/config.rasi ~/.config/rofi/powermenu/powerconfig.rasi
 feh --bg-fill  ~/Pictures/$PICTURE
