0f8f645fabd4fec2cbfedd13599e89ac47d22d54
krm <73038148+3xg3lin@users.noreply.github.com>
2023-09-23 00:08:59 +0300

    Update bg-changer.sh

--- .config/bg-changer.sh
+++ .config/bg-changer.sh
@@ -4,3 +4,4 @@ WHICHPIC=$(cat ~/.config/rofi/config.rasi|grep -o 'guweiz[0-9]*.jpg')
 sed -i "s/$WHICHPIC/$PICTURE/g" ~/.config/rofi/config.rasi ~/.config/rofi/powermenu/powerconfig.rasi
 feh --bg-fill  ~/Pictures/$PICTURE
 betterlockscreen -u ~/Pictures/$PICTURE
+echo "$HOME/Pictures/$PICTURE" > $HOME/.cache/wal/wal
