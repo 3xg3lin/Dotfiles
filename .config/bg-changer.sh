89cbb4e040073a65ce3ac7fb942405a47d3894ae
krm <73038148+3xg3lin@users.noreply.github.com>
2023-09-23 21:23:08 +0300

    Update bg-changer.sh

--- .config/bg-changer.sh
+++ .config/bg-changer.sh
@@ -9,3 +9,4 @@ wal -i ~/Pictures/$PICTURE -q
 PRIMARY=$(grep -i "color1='#[0-9,A-Z,a-z]*'" ~/.cache/wal/colors.sh|grep -o "#[0-9,A-Z,a-z]*")
 sed -i -e "s/%{F#[0-9,A-Z]*}/%{F$PRIMARY}/g" $HOME/.config/polybar/config.ini
 polybar-msg cmd restart
+picom &
