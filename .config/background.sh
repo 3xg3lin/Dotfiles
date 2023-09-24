140f994129a9a803dad5fbb2066e318a394d64a5
krm <73038148+3xg3lin@users.noreply.github.com>
2023-09-24 14:57:12 +0300

    Update background.sh

--- .config/background.sh
+++ .config/background.sh
@@ -4,4 +4,4 @@ WHICHPIC=$(cat ~/.config/rofi/config.rasi|grep -o 'guweiz[0-9]*.jpg')
 sed -i "s/$WHICHPIC/$PICTURE/g" ~/.config/rofi/config.rasi ~/.config/rofi/powermenu/powerconfig.rasi
 feh --bg-fill  ~/Pictures/$PICTURE
 betterlockscreen -u ~/Pictures/$PICTURE
-wal -i ~/Pictures/$PICTURE -q
+wal -i ~/Pictures/$PICTURE -q && $HOME/.config/polybar/walpoly.sh 
