bbdc08bcb3653684f753bde5ef9cc464be1c3e00
kerem <73038148+3xg3lin@users.noreply.github.com>
2023-09-25 15:25:53 +0300

    Update background.sh

--- .config/background.sh
+++ .config/background.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 PICTURE=$(cat /etc/lightdm/lightdm-gtk-greeter.conf |grep -o "guweiz[0-9]*.jpg")
 WHICHPIC=$(cat ~/.config/rofi/config.rasi|grep -o 'guweiz[0-9]*.jpg')
+wal -i ~/Pictures/$PICTURE -q && $HOME/.config/polybar/walpoly.sh 
 sed -i "s/$WHICHPIC/$PICTURE/g" ~/.config/rofi/config.rasi ~/.config/rofi/powermenu/powerconfig.rasi
 feh --bg-fill  ~/Pictures/$PICTURE
 betterlockscreen -u ~/Pictures/$PICTURE
-wal -i ~/Pictures/$PICTURE -q && $HOME/.config/polybar/walpoly.sh 
