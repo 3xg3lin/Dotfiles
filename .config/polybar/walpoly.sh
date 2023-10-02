c70ad3d4a5304ab76be8b8c6380c9a63c55adcb2
kerem <73038148+3xg3lin@users.noreply.github.com>
2023-10-02 15:23:30 +0300

    Update walpoly.sh

--- .config/polybar/walpoly.sh
+++ .config/polybar/walpoly.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 PRIMARY=$(grep -i "color1='#[0-9,A-Z,a-z]*'" ~/.cache/wal/colors.sh|grep -o "#[0-9,A-Z,a-z]*")
 sed -i -e "s/%{F#[0-9,A-Z]*}/%{F$PRIMARY}/g" $HOME/.config/polybar/config.ini
-polybar-msg cmd restart
+$HOME/.config/polybar/launch.sh
