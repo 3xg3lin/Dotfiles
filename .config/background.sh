a031375c664d9ebddbd5b4becd44192bfd76593d
3xg3lin <krmutku76@gmail.com>
2023-10-30 22:56:37 +0300

    Update background.sh

--- .config/background.sh
+++ .config/background.sh
@@ -3,8 +3,8 @@ PICTURE=$(cat /etc/lightdm/lightdm-gtk-greeter.conf |grep -o "guweiz[0-9]*.jpg")
 WHICHPIC=$(cat ~/.config/rofi/config.rasi|grep -o 'guweiz[0-9]*.jpg')
 wal -i ~/Pictures/$PICTURE -q && $HOME/.config/polybar/walpoly.sh 
 COLORID=$(cat $HOME/.cache/wal/colors.sh |grep -o "color1='#[0-9,A-Z]*'"|grep -o '#[0-9,A-Z]*')
-sed -i "323s/#[0-9,A-Z]*/$COLORID/" .config/dunst/dunstrc
+sed -i "323s/#[0-9,A-Z,a-z]*/$COLORID/" .config/dunst/dunstrc
 sed -i "s/$WHICHPIC/$PICTURE/g" ~/.config/rofi/config.rasi ~/.config/rofi/powermenu/powerconfig.rasi
 feh --bg-fill  ~/Pictures/$PICTURE
-dunst &
+systemctl restart --user dunst.service &
 betterlockscreen -u ~/Pictures/$PICTURE
