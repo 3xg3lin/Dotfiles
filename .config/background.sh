b91a2a182b75d4137716e81a31218817e3e7cfff
3xg3lin <krmutku76@gmail.com>
2023-12-03 15:03:34 +0300

    Update background.sh

--- .config/background.sh
+++ .config/background.sh
@@ -5,6 +5,7 @@ wal -i ~/Pictures/$PICTURE -q && $HOME/.config/polybar/walpoly.sh
 COLORID=$(cat $HOME/.cache/wal/colors.sh |grep -o "color1='#[0-9,A-Z]*'"|grep -o '#[0-9,A-Z]*')
 sed -i "323s/#[0-9,A-Z,a-z]*/$COLORID/" .config/dunst/dunstrc
 sed -i "s/$WHICHPIC/$PICTURE/g" ~/.config/rofi/config.rasi ~/.config/rofi/powermenu/powerconfig.rasi
-feh --bg-fill  ~/Pictures/$PICTURE
+#feh --bg-fill  ~/Pictures/$PICTURE
+feh --bg-fill ~/Pictures/black-screen.jpg
 systemctl restart --user dunst.service &
 betterlockscreen -u ~/Pictures/$PICTURE
