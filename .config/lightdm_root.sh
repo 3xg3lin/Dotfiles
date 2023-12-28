55d850bd874bcea6ad5a966fdaebedac1d513d0e
kerem <73038148+3xg3lin@users.noreply.github.com>
2023-12-28 13:20:56 +0300

    Update lightdm_root.sh

--- .config/lightdm_root.sh
+++ .config/lightdm_root.sh
@@ -2,3 +2,4 @@
 PICTURE=$(ls /usr/share/backgrounds/archlinux/ |shuf -n 1)
 WHICHPIC=$(cat /etc/lightdm/lightdm-gtk-greeter.conf|grep -o 'guweiz[0-9]*.jpg')
 sed -i "s/$WHICHPIC/$PICTURE/g" /etc/lightdm/lightdm-gtk-greeter.conf
+#don't foget to add greeter-setup-script in lightdm.conf
